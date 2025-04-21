e5df836dc0fe8ae65248df4ae8932c18a567c5b2
Gurra Berg <fnuf_links@hotmail.com>
2025-04-21 16:03:04 +0200

    Merge local project with GitHub repo

---
+++ Areas/Identity/Data/GameVaultAppUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace GameVaultApp.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the GameVaultAppUser class
+public class GameVaultAppUser : IdentityUser
+{
+    [PersonalData]
+    public string FirstName { get; set; }
+
+    [PersonalData]
+    public string LastName { get; set; }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using GameVaultApp.Data;
+using GameVaultApp.Areas.Identity.Data;
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("GameVaultAppContextConnection") ?? throw new InvalidOperationException("Connection string 'GameVaultAppContextConnection' not found.");
+
+builder.Services.AddDbContext<GameVaultAppContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<GameVaultAppUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<GameVaultAppContext>();
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
